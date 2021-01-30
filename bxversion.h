e4dc745b75e5489bb0ca9c10c2fb1fd445a2584b
hartnel <kritikostamghuo@gmail.com>
2021-01-30 20:43:59 +0100

    init from bochs source code

---
+++ bxversion.h
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////////////////////////////////
+// This file is checked in as bxversion.h.in.  The configure script
+// substitutes variables and creates bxversion.h.
+/////////////////////////////////////////////////////////////////////////
+
+#define VERSION "2.6.10"
+#define VER_MAJOR "2"
+#define VER_MINOR "6"
+#define VER_REVISION "10"
+#define VER_SVN   "0"
+#define REL_STRING "Built from SVN snapshot on December 1, 2019"
